c25afc39e28af2bef39f980658462a8b4be84943
Lucas Barbosa <lucasbarbosa7@hotmail.com>
2022-05-14 21:11:38 -0300

    Maiores detalhes!
    Além de comentar mais o código, agradeci a Loiane!

--- meuAplicativo/src/app/app.module.ts
+++ meuAplicativo/src/app/app.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
-import { MeuPrimeiroComponent} from './meu-primeiro/meu-primeiro.component';
+import { AppComponent } from './app.component';// O próprio "AppComponent", que está na mesma pasta, também precisa ser importado.
+import { MeuPrimeiroComponent} from './meu-primeiro/meu-primeiro.component';// Além da classe exportada no arquivo TypeScript dentro do próprio componente, ele precisa ser importado no "app.module" para que o Angular possa utilizar o componente.
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MeuPrimeiroComponent,
+    AppComponent,// Além de importado, declarado. Caso não, fica inutilizável.
+    MeuPrimeiroComponent,// Uma vez importada, deve ser declarada nas "declarations".
     MeuPrimeiro2Component
   ],
   imports: [
--- meuAplicativo/src/app/meu-primeiro/meu-primeiro.component.ts
+++ meuAplicativo/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core';//Importanto a classe "Component" do pacote "/core" dentro de"Angular"
 
-@Component({
-    selector: 'meu-primeiro-component',
+@Component({// Falando para o compilador do Typescript que a classe que declarei abaixo é um "Component"
+    selector: 'meu-primeiro-component', // A chave "selector" é responsável por determinar qual seletor "<>" HTML irá selecionar meu componente. À sua frente, segue o valor, que acredito que não pode ser uma palavra já utilizada pelo HTML, como h1, por exemplo.
     template: `
         <p>Meu primeiro component com Angular 2!</p>
     `
-})
-export class MeuPrimeiroComponent {
+})// A chave "template" é responsável por armazenar diretamente no seletor o seu conteúdo. Neste uso, é feito a utilização do Template String. Ele facilita interpolação.
+export class MeuPrimeiroComponent {// É aqui que declaro minha classe, usando Upper Cammel Case. A propriedade "export" é responsável por tornar a classe vísivel para outros arquivos.
 
 }
